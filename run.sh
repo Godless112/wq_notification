e1ef1445a39cc3eeb464b81aeb1c52af15b3fae6
Godless112 <parki.steam@gmail.com>
2024-11-22 16:43:38 +0100

    run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 
 cd /home/me/dev/wow_notifications
 
+https://discord.com/api/webhooks/1309541071155630111/Wqzkk_WoHTlqxCr2_-cv3895MFBUPiVb4RW6A-Gr2wAbk88TTaWLKxzKmGrLNO0drphx
+
 source venv/bin/activate
 python main.py
